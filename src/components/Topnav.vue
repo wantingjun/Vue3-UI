915da7a0963e028a863b043ab991c6648cacd76a
TingjunWan <wtj9631@163.com>
2021-04-25 21:10:04 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleMenu">LOGO</div>
+        <div class="logo" >LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
-        <span class ="toggleIcon"></span>
+        <span class ="toggleIcon" @click="toggleMenu"></span>
     </div>
 </template>
 
@@ -33,6 +33,7 @@
         position:relative;
         z-index:10;
         justify-content: center;
+        align-items: center;
         > .logo {
             max-width: 6em;
             margin-right: auto;
@@ -46,9 +47,20 @@
                 margin: 0 1em;
             }
         }
+        > .toggleIcon{
+            width:24px;
+            height:24px;
+            background: red;
+            position:absolute;
+            left:16px;
+            top:50%;
+            transform: translateY(-50%);
+            display:none;
+        }
          @media (max-width:500px){
              > .menu{display:none;}
              > .logo{margin:0 auto}
+             > .toggleIcon{display:inline-block}
          }
 
     }
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -44,10 +44,6 @@
         background: lightblue;
         width: 150px;
         padding: 16px;
-        position: fixed;
-        padding-top:70px;
-        top: 0;
-        left: 0;
         > h2 {
             margin-bottom: 4px;
         }
@@ -55,6 +51,12 @@
             > li {
                 padding: 4px 0;
             }
+        }
+        @media (max-width:500px){
+            position: fixed;
+            top:0;
+            left:0;
+            padding-top:70px;
         }
     }
 </style>
