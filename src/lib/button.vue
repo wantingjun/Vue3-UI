712119d4efbddd6e820a0e310bab2691e345e087
wantingjun <wtj9631@gamil.com>
2021-04-26 11:06:00 +0800

    添加theme属性“

--- src/lib/button.vue
+++ src/lib/button.vue
@@ -1,19 +1,18 @@
 <template>
-    <div style="border:1px solid red" :size="size">
-    <button v-bind="rest">
+    <button calss="gulu-button" :class="`theme-${theme}`">
         <slot></slot>
     </button>
-    </div>
+
 </template>
 
 <script lang="ts">
     export default {
         name: "button",
-        inheritAttrs:false ,//1. 不让div继承：继承属性，div就不在拥有外面importbutton绑定的继承事件了
-        setup(props, context){
-            const {size,...rest} =context.attrs //rest是除了size其他的
-            return {size,rest}
-
+        props:{
+            theme:{
+                type:String,
+                default:'button'
+            }
         }
     }
 </script>
