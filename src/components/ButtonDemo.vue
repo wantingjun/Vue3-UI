c729a9ba0c1acc887a3fa9033ac2d29299740621
wantingjun <wtj9631@gamil.com>
2021-04-26 15:06:52 +0800

    button size css 2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,17 +1,28 @@
 <template>
-    <div>Button示例</div>
-    <h1> 示例1</h1>
+    <h1>示例1</h1>
     <div>
-        <Button >你好</Button>
+        <Button>你好</Button>
         <Button theme="button">你好</Button>
         <Button theme="link">你好</Button>
         <Button theme="text">你好</Button>
     </div>
-    <h1> 示例2</h1>
+    <h1>示例2</h1>
     <div>
-        <Button >normal</Button>
-        <Button size="big">dadada</Button>
-        <Button size="small">small</Button>
+        <div>
+            <Button size="big">大大大</Button>
+            <Button>普普通</Button>
+            <Button size="small">小小小</Button>
+        </div>
+        <div>
+            <Button theme="link" size="big">大大大</Button>
+            <Button theme="link">普普通</Button>
+            <Button size="small" theme="link">小小小</Button>
+        </div>
+        <div>
+            <Button size="big" theme="text">大大大</Button>
+            <Button theme="text">普普通</Button>
+            <Button size="small" theme="text">小小小</Button>
+        </div>
     </div>
 
 </template>
