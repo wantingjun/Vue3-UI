389682da6143d3f90ce2ddf5be341a7eddd6ed31
wantingjun <wtj9631@gamil.com>
2021-04-28 14:36:57 +0800

    优化1

--- src/lib/tabs.vue
+++ src/lib/tabs.vue
@@ -2,7 +2,7 @@
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav" ref="container">
             <div class="gulu-tabs-nav-item" v-for="(t,index) in titles"
-                 :ref="el=>{if(el) navItems[index]=el}"
+                 :ref="el=>{if(t === selected) selectedItem = el}"
                  @click="select(t)"
                  :class="{selected: t=== selected}" :key="index">{{t}}</div>
             <div class="gulu-tabs-nav-indicator"
@@ -25,18 +25,17 @@
         },
 
         setup(props,context){
-            const navItems = ref<HTMLDivElement[]>([])
+           // const navItems = ref<HTMLDivElement[]>([])
+            const selectedItem = ref<HTMLDivElement>(null) //被选中的item
             const indicator = ref<HTMLDivElement>(null)
             const container = ref<HTMLDivElement>(null)
             const x=()=>{
-                console.log({...navItems.value})
-                const divs = navItems.value
-                const result = divs.filter(div=>div.classList.contains('selected'))[0] //被选中的tab filter返回数组，所以获取[0]
-                console.log(result)
-                const {width} = result.getBoundingClientRect()
+               // const divs = navItems.value
+              //  const result = divs.filter(div=>div.classList.contains('selected'))[0] //被选中的tab .filter返回数组，所以获取[0]
+                const {width} = selectedItem.value.getBoundingClientRect()
                 indicator.value.style.width = width + 'px'
                 const {left:left1} = container.value.getBoundingClientRect() //获取container的left
-                const {left:left2} = result.getBoundingClientRect() //result 的left
+                const {left:left2} = selectedItem.value.getBoundingClientRect() //result 的left
                 const left = left2 - left1
                 indicator.value.style.left = left + 'px'
             }
@@ -64,7 +63,7 @@
             }
 
 
-            return {defaults,titles,current,select,navItems,indicator,container}
+            return {defaults,titles,current,select,indicator,container,selectedItem}
         }
 
     }
