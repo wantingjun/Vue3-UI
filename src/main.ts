956279577d536ad366d4e533d6af99aab67d7b82
wantingjun <wtj9631@gamil.com>
2021-04-30 15:20:51 +0800

    引入github-markdown-css

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import {router} from './router.ts'
-
+import 'github-markdown-css'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
