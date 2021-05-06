3241898bc3d860b66a32011d0eda96a5965fdccd
TingjunWan <wtj9631@163.com>
2021-05-06 22:22:11 +0800

    优化改造dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,52 +1,20 @@
 <template>
-    <div>dialog实例</div>
-    <h1>1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="x" :ok="f1" :cancel="f2">
-        <template v-slot:content>
-            <div>hi</div>
-        </template>
-        <template v-slot:title>
-            <strong>加醋的title</strong>
-        </template>
-
-    </Dialog>
-    <h1>实例2</h1>
-    <Button @click="showDialog">showDialog</Button>
+    <div>Dialog 示例</div>
+    <Demo :component="Dialog1Demo" />
+    <Demo :component="Dialog2Demo" />
 </template>
 
 <script lang="ts">
-    import Dialog from '../lib/Dialog.vue'
-    import Button from '../lib/Button.vue'
-    import {ref} from 'vue'
-    import {openDialog} from '../lib/openDialog.ts'
+    import Demo from './demo.vue';
+    import Dialog1Demo from './dialog1.demo.vue';
+    import Dialog2Demo from './dialog2.demo.vue';
     export default {
         name: "DialogDemo",
-        components:{Dialog,Button},
+        components:{Demo},
         setup(){
-            const x =ref(false)
-            const toggle = ()=>{
-                console.log(x)
-                x.value =!x.value
-            }
-            const f1=()=>{
-                return true
-            }
-            const f2=()=>{
-            }
-            const showDialog=()=>{
-                openDialog({
-                    title:'标题',
-                    content:'HELLO',
-                    ok(){
-                        console.log('ok')
-                    },
-                    cancel(){
-                        console.log('cancel')
-                    }
-                })
-            }
-            return {x,toggle,f1,f2,showDialog}
+            return {
+                Dialog1Demo,
+                Dialog2Demo}
         }
     }
 </script>
