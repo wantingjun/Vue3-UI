8a1ab13f41bc031607c5bf6aefc21614709980cc
TingjunWan <wtj9631@163.com>
2021-05-06 22:15:31 +0800

    优改造button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,73 +1,34 @@
 <template>
-    <h1>示例1</h1>
     <div>
-        <Button>你好</Button>
-        <Button theme="button">你好</Button>
-        <Button theme="link">你好</Button>
-        <Button theme="text">你好</Button>
+        <div>Button 示例</div>
+        <Demo :component="Button1Demo" />
+        <Demo :component="Button2Demo" />
+        <Demo :component="Button3Demo" />
+        <Demo :component="Button4Demo" />
+        <Demo :component="Button5Demo" />
     </div>
-    <h1>示例2</h1>
-    <div>
-        <div>
-            <Button size="big">大大大</Button>
-            <Button>普普通</Button>
-            <Button size="small">小小小</Button>
-        </div>
-        <div>
-            <Button theme="link" size="big">大大大</Button>
-            <Button theme="link">普普通</Button>
-            <Button size="small" theme="link">小小小</Button>
-        </div>
-        <div>
-            <Button size="big" theme="text">大大大</Button>
-            <Button theme="text">普普通</Button>
-            <Button size="small" theme="text">小小小</Button>
-        </div>
-    </div>
-    <h1>示例3</h1>
-    <div>
-        <div>
-            <h3>button</h3>
-            <Button level="main">主要按钮</Button>
-            <Button>普通按钮</Button>
-            <Button level="danger">危险按钮</Button>
-            </div>
-            <div>
-                <h3>link</h3>
-                <Button theme="link" level="main">主要链接按钮</Button>
-                <Button theme="link">普通链接按钮</Button>
-                <Button theme="link" level="danger">危险链接按钮</Button>
-            </div>
-            <div>
-                <h3>text</h3>
-                <Button theme="text" level="main">主要文字按钮</Button>
-                <Button theme="text">普通文字按钮</Button>
-                <Button theme="text" level="danger">危险文字按钮</Button>
-            </div>
-        </div>
-    <h1>示例4</h1>
-    <div>
-        <Button disabled>禁用按钮</Button>
-        <Button theme="link" disabled>禁用链接按钮</Button>
-        <Button theme="text" disabled>禁用按钮</Button>
-    </div>
-    <h1>示例5 loading</h1>
-    <Button loading>loading</Button>
-    <Button >加载完毕</Button>
-
 
 </template>
 
 <script lang="ts">
     import Button from '../lib/button.vue'
+    import Demo from "./demo.vue";
+    import Button1Demo from "./button1.demo.vue";
+    import Button2Demo from "./button2.demo.vue";
+    import Button3Demo from "./button3.demo.vue";
+    import Button4Demo from "./button4.demo.vue";
+    import Button5Demo from "./button5.demo.vue";
     export default {
         name: "ButtonDemo",
-        components:{Button},
+        components:{Button, Demo},
         setup(){
-            const onClick = ()=>{
-                console.log('hi')
-            }
-            return {onClick}
+            return {
+                Button1Demo,
+                Button2Demo,
+                Button3Demo,
+                Button4Demo,
+                Button5Demo
+            };
         }
     }
 </script>
