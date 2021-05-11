1b64e55a07e74ba3b139d774225e3608431a8f0a
TingjunWan <wtj9631@163.com>
2021-05-11 22:27:55 +0800

    解决build后，makrdown出错的bug

--- src/router.ts
+++ src/router.ts
@@ -5,20 +5,23 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import intro from './markdown/intro.md'
+import install from './markdown/install.md'
+import start from './markdown/start.md'
 
 import { h } from 'vue';
 import Markdown from './components/markdown.vue';
 const history = createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
     history: history,
     routes: [
         {path: '/', component: Home},
         {path: '/doc', component: Doc,children:[
                 {path:'',redirect:'/doc/intro'},
-                {path:'intro',component: md('intro')},
-                {path:'start',component: md('start')},
-                {path:'install',component: md('install')},
+                {path:'intro',component: md(intro)},
+                {path:'start',component: md(start)},
+                {path:'install',component: md(install)},
                 {path:'switch',component:SwitchDemo},
                 {path:'button',component:ButtonDemo},
                 {path:'dialog',component:DialogDemo},
