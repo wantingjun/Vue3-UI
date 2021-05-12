1d7ea39ca85202c9772d481c24c4d6871195e8b7
TingjunWan <wtj9631@163.com>
2021-05-12 21:36:41 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { default as Switch } from './switchwitch.vue';
+export { default as Button } from './buttonutton.vue';
+export { default as Tabs } from './tabs.vue';
+export { default as Tab } from './tab.vue';
+export { default as Dialog } from './dialog.vue';
+export { openDialog as openDialog } from './openDialog';
