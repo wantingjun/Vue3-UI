7d1b6739e8937ee7e2df26a5e95b8462c868cd70
TingjunWan <wtj9631@163.com>
2021-05-12 21:41:48 +0800

    修复lib/index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
-export { default as Switch } from './switchwitch.vue';
-export { default as Button } from './buttonutton.vue';
+export { default as Switch } from './switch.vue';
+export { default as Button } from './button.vue';
 export { default as Tabs } from './tabs.vue';
 export { default as Tab } from './tab.vue';
 export { default as Dialog } from './dialog.vue';
